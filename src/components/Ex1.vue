75253ef01322d52ac8a905b7df08bdf2ba235977
wztan3590 <weizhi.tan.2024@computing.smu.edu.sg>
2025-10-22 01:36:27 +0000

    commit

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,14 +1,31 @@
 <script>
     export default { 
-        // Add Code Here to complete the task
-        // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+    data() {
+        return {
+            x: 0,
+            y: 0,
+            operators: ["+", "-", "*", "/", "%"],
+            selectedOp: ""
+        }
+    },
+    computed: {
+        result() {
+            switch(this.selectedOp) {
+                case "+": return this.x + this.y;
+                case "-": return this.x - this.y;
+                case "*": return this.x * this.y;
+                case "/": return this.x / this.y;
+                case "%": return this.x % this.y;
+            }
+        }
     }
+}
 </script>
 
 <template>
     <p>x <input v-model.number="x"></p>
     <select v-model="selectedOp">
-        <option v-for="op in operators" >{{ op }}</option>
+        <option v-for="op in operators">{{ op }}</option>
     </select>
     <p>y <input v-model.number="y"></p>
     
